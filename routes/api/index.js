ba6bcfeec5505a6c5700de3046e1a0ce74979d4a
hiren patel <hirenpatel0526@yahoo.com>
2022-11-05 13:17:50 -0700

    index js api done

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require ('express').Router();
+
+
+const userRoute = require('./userRoute');
+const thoughtRoute = require('./thoughtRoute');
+
+router.use('./thoughts', thoughtRoute);
+
+module.exports = router;
