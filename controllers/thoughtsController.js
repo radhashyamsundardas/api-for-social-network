f428f9a51d8b3d5a3c0ca37f37f18f245304ce6f
hiren patel <hirenpatel0526@yahoo.com>
2022-11-08 11:17:50 -0800

    done with gethoughtid

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -10,5 +10,21 @@ getAllthoughts(req,res){
         res.status(500).json(err);
     })
 },
+getThougtId({params}, res){
+    thought.findOne({_id: params.id})
+    .populate({path: 'reaction', select: '-__v'})
+    .select('-__v')
+    .then(dbThoughtINfo =>{
+        if (!dbThoughtINfo){
+            res.status(404).json({message: 'no thought with this id'});
+            return;
+        }
+        res.json(dbThoughtINfo)
+
+})
+.catch(err => {
+    console.log(err);res.status(400).json(err);
+});
+},
 
 }
