e0c5d834ff0c0f683d3f966307659e6707878b5e
hiren patel <hirenpatel0526@yahoo.com>
2022-11-07 19:18:27 -0800

    finished model user

--- models/user.js
+++ models/user.js
@@ -8,6 +8,37 @@ const usrSchema = new Schema ({
         required: [true, 'userme cannot be empty'],
         trim: true
     },
+    email: {
+        type: String,
+        unique:[true, 'email is in use'],
+        required: [true, 'email cannot be empty'],
+        match: [
+        /^([a-z 0-9_\.-]+)@([\dc-z\.-]+)\.([a-z\.]{1,5})$/
+        ]
+    },
+    thoughts:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:'thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+},
+{
+    toJSON:{
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+}
+)
+
+usrSchema.virtual;('friendCount').get(function(){
+    return this.friends.length;
 })
+
+const user = model('user', usrSchema);
 
 module.exports = user;
