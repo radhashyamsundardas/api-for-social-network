684af60b20f6553afe56c66e878708dfa47dd338
hiren patel <hirenpatel0526@yahoo.com>
2022-11-08 17:12:21 -0800

    added getuserbyid

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,5 +6,22 @@ const userController = {
             res.status(500).json(err);
         })
     },
+    getUserById ({params}, res){
+        User.findOne({ _id: params.id})
+        .populate([
+            {path: 'thoughts', select: '-__v'},
+            {path: 'friends', select: '-__v'}
+        ])
+        .select('-__v')
+        .then(dbUserinfo => {
+            if (!dbUserinfo){
+                res.status(400).json({message: 'user not found with id'});
+                return;
+            }
+            res.json(dbUserinfo);
+        })
+    },
+
+
     
 }
